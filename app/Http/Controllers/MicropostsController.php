4211c9ae66dd80437bd3c6518fa30193ab0c509a
mio.mio <omochi@techacademy.jp>
2018-06-15 05:16:06 +0000

    follow,unfollow

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -6,24 +6,30 @@ use Illuminate\Http\Request;
 
 class MicropostsController extends Controller
 {
-   public function index()
+   
+     public function index()
     {
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
+             $count_microposts = $user->microposts()->count();
+             $count_followings = $user->followings()->count();
+             $count_followers = $user->followers()->count();
 
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
+                'count_microposts' =>$count_microposts,
+                'count_followings' => $count_followings,
+            'count_followers' => $count_followers,
             ];
-            $data += $this->counts($user);
-            return view('users.show', $data);
-        }else {
-            return view('welcome');
+            return view('users.show',$data);
         }
-    }
+        else{return view('welcome');
     
+    }
+    }
     public function store(Request $request)
     {
         $this->validate($request, [
